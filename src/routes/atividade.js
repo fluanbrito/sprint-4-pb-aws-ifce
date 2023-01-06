c28e9abfe7ec345b55201f8a0538911c632471bf
Herisson Hyan <herissonhyan@gmail.com>
2023-01-06 12:35:26 -0300

    Criação e organização da pasta controllers

--- src/routes/atividade.js
+++ src/routes/atividade.js
@@ -1,37 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const axios = require("axios")
-const { v4: uuidv4 } = require('uuid');
 
-function createJsonAT(id, atividades, tipo, participantes, acessibilidade) {
-    let template = `{
-        "id": "${id}",
-        "atividade": "${atividades}",
-        "tipo": "${tipo}",
-        "participantes": ${participantes},
-        "acessibilidade": "${acessibilidade}"
-      }
-    `
-    tempJson = JSON.parse(template)
-    return tempJson
-};
+const AtividadeController = require("../controllers/AtividadeController.js")
 
-function formatAcc(acc) {
-    newAcc = `${(parseFloat(acc)) * 100}%`
-    return newAcc
-};
 
-router.get('/api/atividades', async (req, res) => {
-    const { data } = await axios("https://www.boredapi.com/api/activity")
-    const id = uuidv4()
-    const atividades = data.activity
-    const tipo = data.type
-    const participantes = data.participants
-    const acessibilidade = formatAcc(data.accessibility)
-
-    const returnValueAT = createJsonAT(id, atividades, tipo, participantes, acessibilidade)
-
-    res.send(returnValueAT)
-})
+router.get('/api/atividades', AtividadeController.atJson)
 
 module.exports = router
--- src/routes/chucknorris.js
+++ src/routes/chucknorris.js
@@ -1,47 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const axios = require("axios")
-const { v4: uuidv4 } = require('uuid');
 
+const CnController = require("../controllers/CnController.js")
 
-function formatdate(date) {
-    datef = date.slice(0, 10).split('-');
-    return `${datef[2]}-${datef[1]}-${datef[0]}`
-};
+router.get('/api/piadas', CnController.cnJson);
 
-function formatevalue(value) {
-    newValue = value.replace('Chuck Norris', 'Chuck Norris'.toLocaleUpperCase())
-    return newValue
-};
-function formatePiada(value) {
-    newValue = value.replace(/"/g, '`')
-    return newValue
-};
-function createJsonCN(dateupdate, datecreate, icon, id, piada, referencia) {
-    let template = `{
-        "data_atualizacao": "${dateupdate}",
-            "data_criacao": "${datecreate}",
-                "icone": "${icon}",
-                    "id": "${id}",
-                        "piada": "${piada}",
-                            "referencia": "${referencia}"
-    }
-    `
-    tempJson = JSON.parse(template)
-    return tempJson
-};
-
-router.get('/api/piadas', async (req, res) => {
-    const { data } = await axios("https://api.chucknorris.io/jokes/random")
-    const dateupdate = formatdate((data.updated_at))
-    const datecreate = formatdate((data.created_at))
-    const icon = data.icon_url
-    const id = uuidv4();
-    const piada = formatePiada(formatevalue(data.value));
-    const referencia = data.url
-
-    const returnValueCN = createJsonCN(dateupdate, datecreate, icon, id, piada, referencia)
-
-    res.send(returnValueCN)
-})
 module.exports = router
