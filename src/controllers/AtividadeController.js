ebc171d2bca20e76ebd835cffedc285b78f89002
Herisson Hyan <herissonhyan@gmail.com>
2023-01-11 15:08:23 -0300

    Tratamento de erros.

--- src/controllers/AtividadeController.js
+++ src/controllers/AtividadeController.js
@@ -4,14 +4,19 @@ const { v4: uuidv4 } = require('uuid');
 const func = require("../functions/Atfunctions.js")
 
 exports.atJson = async (req, res) => {
-    const { data } = await axios("https://www.boredapi.com/api/activity")
-    const id = uuidv4()
-    const atividades = data.activity
-    const tipo = data.type
-    const participantes = data.participants
-    const acessibilidade = func.formatAcc(data.accessibility)
+    try {
+        const { data } = await axios("https://www.boredapi.com/api/activity")
+        const id = uuidv4()
+        const atividades = data.activity
+        const tipo = data.type
+        const participantes = data.participants
+        const acessibilidade = func.formatAcc(data.accessibility)
 
-    const returnValueAT = func.createJsonAT(id, atividades, tipo, participantes, acessibilidade)
+        const returnValueAT = func.createJsonAT(id, atividades, tipo, participantes, acessibilidade)
 
-    res.send(returnValueAT)
+        res.send(returnValueAT)
+    } catch (error) {
+        res.send({ error: error.message })
+    }
+
 }
--- src/controllers/CnController.js
+++ src/controllers/CnController.js
@@ -6,15 +6,19 @@ const { v4: uuidv4 } = require('uuid');
 const func = require("../functions/Cnfunction.js")
 
 exports.cnJson = async (req, res) => {
-    const { data } = await axios("https://api.chucknorris.io/jokes/random")
-    const dateupdate = func.formatdate((data.updated_at))
-    const datecreate = func.formatdate((data.created_at))
-    const icon = data.icon_url
-    const id = uuidv4();
-    const piada = func.formatePiada(func.formatevalue(data.value));
-    const referencia = data.url
+    try {
+        const { data } = await axios("https://api.chucknorris.io/jokes/random")
+        const dateupdate = func.formatdate((data.updated_at))
+        const datecreate = func.formatdate((data.created_at))
+        const icon = data.icon_url
+        const id = uuidv4();
+        const piada = func.formatePiada(func.formatevalue(data.value));
+        const referencia = data.url
 
-    const returnValueCN = func.createJsonCN(dateupdate, datecreate, icon, id, piada, referencia)
+        const returnValueCN = func.createJsonCN(dateupdate, datecreate, icon, id, piada, referencia)
 
-    res.send(returnValueCN)
+        res.send(returnValueCN)
+    } catch (error) {
+        res.send({ error: error.message })
+    }
 }
