d2041badf68b6113626cce31b8dd6cb191f469e0
mylensoares <mylenateles@hotmail.com>
2023-01-12 22:08:22 -0300

    modificando guid

--- src/formatacoes.js
+++ src/formatacoes.js
@@ -5,12 +5,10 @@ module.exports = {
         return date
     },
 
-    // Realizar verificações nessa função
     geraGUID(){
-        function S4() {
-            return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
-        }
-        return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+        const { Guid } = require('js-guid');
+        let guid = new Guid().toString()
+        return guid
     },
 
     caixaAlta(frase){
