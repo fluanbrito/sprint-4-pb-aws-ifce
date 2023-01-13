7261afbf9ac86a65e8d5126e051a3a2d4ce6fd5c
Kurokishin <rafaelpereirasouza15@gmail.com>
2023-01-13 09:08:05 -0300

    Tratamento de erros

--- src/routes/atividades.js
+++ src/routes/atividades.js
@@ -5,17 +5,22 @@ const axios = require('axios');
 const format = require("../formatacoes")
 
 router.get('/', async(req, res) => {
-    const {data} = await axios("https://www.boredapi.com/api/activity")
-    // console.log(data) exibe todos os dados de retorno da API
-    console.log(JSON.parse(`
-    {
-       "id": "${format.geraGUID()}",
-       "atividade": "${data.activity}",
-       "tipo": "${data.type}",
-       "participantes": "${data.participants}",
-       "acessibilidade": "${format.acessibilidade(data.accessibility)}"
-    }`));
-    res.end()
+    try {
+        const {data} = await axios("https://www.boredapi.com/api/activity")
+        // console.log(data) exibe todos os dados de retorno da API
+        console.log(JSON.parse(`
+        {
+        "id": "${format.geraGUID()}",
+        "atividade": "${data.activity}",
+        "tipo": "${data.type}",
+        "participantes": "${data.participants}",
+        "acessibilidade": "${format.acessibilidade(data.accessibility)}"
+
+        }`));
+        res.end();
+    } catch (error) {
+        res.send({error: error.message});
+    }
 });
 
 module.exports = router;
--- src/routes/piadas.js
+++ src/routes/piadas.js
@@ -5,18 +5,22 @@ const axios = require('axios');
 const format = require("../formatacoes")
 
 router.get('/', async(req, res) => {
-    const {data} = await axios("https://api.chucknorris.io/jokes/random")
-    // console.log(data) exibe todos os dados de retorno da API
-    console.log(JSON.parse(`
-    { 
-       "data_atualizacao": "${format.formatDate(data.updated_at)}",
-       "data_criacao": "${format.formatDate(data.created_at)}",
-       "icon": "${data.icon_url}",
-       "id":" ${format.geraGUID()}",
-       "piada": "${format.caixaAlta(data.value)}",
-       "referencia": "${data.url}"
-    }`));
-    res.end()
+    try {
+        const {data} = await axios("https://api.chucknorris.io/jokes/random")
+        // console.log(data) exibe todos os dados de retorno da API
+        console.log(JSON.parse(`
+        { 
+        "data_atualizacao": "${format.formatDate(data.updated_at)}",
+        "data_criacao": "${format.formatDate(data.created_at)}",
+        "icon": "${data.icon_url}",
+        "id":" ${format.geraGUID()}",
+        "piada": "${format.caixaAlta(data.value)}",
+        "referencia": "${data.url}"
+        }`));
+        res.end()
+    } catch (error) {
+        res.send({error: error.message});
+    }
 });
 
 module.exports = router;
