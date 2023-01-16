ab7a56a5f967deb3e71704540f2f3fc626b5e986
Kurokishin <rafaelpereirasouza15@gmail.com>
2023-01-16 13:38:42 -0300

    Removendo comentários

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const app = express();
-const exphbs = require('express-handlebars');
-const path = require('path');
 
 const PORT = 8080;
 
@@ -9,13 +7,6 @@ const PORT = 8080;
 app.listen(PORT, function() {
    console.log(`O APP está rodando na porta ${PORT}`)
 });
-
-// // Setando as views com handlebars
-// app.set('views', path.join(__dirname, 'views'));
-// app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
-// app.set('view engine', 'handlebars');
-
-// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async(req, res) => {
    // Renderiza a página HTML index
